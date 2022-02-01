e27018812fa5e667ed98f784a0b8aa8dc2a40cf0
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-02-01 19:13:02 +0200

    finished the task

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE anyMovie;
+CREATE TABLE anyMovie(
+
+     id SERIAL PRIMARY KEY,
+     title VARCHAR(255),
+     release_date VARCHAR(255),
+     poster VARCHAR(1000),
+     overview VARCHAR(1000),
+     comment VARCHAR(255)
+
+)
